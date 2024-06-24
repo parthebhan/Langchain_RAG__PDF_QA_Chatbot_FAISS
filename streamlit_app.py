7d5b4e8c8e3a2a9e516719edb4764e2204da8562
Parthebhan Pari <152586277+parthebhan@users.noreply.github.com>
2024-06-25 01:35:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from PyPDF2 import Pdf
+from PyPDF2 import PdfReader
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
