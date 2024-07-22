e8e734dfc65ed436c8536f4f8217c188786e05ed
Parthebhan Pari <152586277+parthebhan@users.noreply.github.com>
2024-07-22 22:47:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,7 @@ def get_conversational_chain():
 
 def user_input(user_question):
     try:
-        embeddings = GoogleGenerativeAIEmbeddings(model=emb)
+        embeddings = GoogleGenerativeAIEmbeddings(model=emb,google_api_key=api_key)
         # Enable dangerous deserialization here
         new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
         docs = new_db.similarity_search(user_question)
