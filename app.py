0979d63d8d68b7a48f716d6825d2e9bb254acff7
Parthebhan Pari <152586277+parthebhan@users.noreply.github.com>
2024-08-21 11:38:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def user_input(user_question):
 
 def main():
     st.set_page_config("Chat PDF")
-    st.header("Stop Searching, Start Talking! Get Answers from PDFs Instantly with AI")
+    st.header("Stoooop Searching, Start Talking! Get Answers from PDFs Instantly with AI")
 
     user_question = st.text_input("Ask a Question from the PDF Files")
 
