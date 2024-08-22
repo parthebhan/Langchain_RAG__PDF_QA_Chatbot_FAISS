3f114838b44b7fc051587aa408e7fa5b45659745
Parthebhan Pari <152586277+parthebhan@users.noreply.github.com>
2024-08-22 15:08:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,21 @@
 import streamlit as st
 from PyPDF2 import PdfReader
+
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
-from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from langchain_groq import ChatGroq
-import google.generativeai as genai
+
 import os
 import time
 import shutil
 from docx import Document
 from io import BytesIO
 
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.chains import ConversationalRetrievalChain
 from transformers import pipeline
 from huggingface_hub import login
-
-from langchain_groq import ChatGroq
 
 groq_api_key = st.secrets["groq_api_key"]
 
